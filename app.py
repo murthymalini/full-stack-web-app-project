d146a127f79e51981dfb38f4fa35eeee3c9cb93c
murthymalini2019 <53497733+murthymalini2019@users.noreply.github.com>
2019-12-18 18:26:52 -0500

    Pushing to Git

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import dns
+# import scrape_craigslist
+
+app = Flask(__name__)
+
+# Use flask_pymongo to set up mongo connection
+# app.config["MONGO_URI"] = "mongodb://localhost:27017/craigslist_app"
+app.config["MONGO_URI"] = "mongodb+srv://Harmeet:harmeet123@cluster0-v6uxh.mongodb.net/complete_data_death"
+mongo = PyMongo(app)
+
+# Or set inline
+# mongo = PyMongo(app, uri="mongodb://localhost:27017/craigslist_app")
+
+
+@app.route("/")
+def index():
+    collection = mongo.db.collection.find_one()
+    return render_template("index.html", collection=collection)
+
+
+# @app.route("/scrape")
+# def scraper():
+#     listings = mongo.db.listings
+#     listings_data = scrape_craigslist.scrape()
+#     listings.update({}, listings_data, upsert=True)
+#     return redirect("/", code=302)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
