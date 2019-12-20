4e3f21846d86707ef439c6e7375655794c111322
Harmeet Kaur <meet.academia@gmail.com>
2019-12-20 08:55:02 -0500

    app.py with 3 routes added

--- paradise/app.py
+++ paradise/app.py
@@ -1,36 +1,59 @@
-# import necessary libraries
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template
+# from flask_table import Table, Col
 from flask_pymongo import PyMongo
+from flask import jsonify
+from flask import request
+import json
+
 import dns
-# import scrape_craigslist
-​
+
 app = Flask(__name__)
-​
-# Use flask_pymongo to set up mongo connection
-# app.config["MONGO_URI"] = "mongodb://localhost:27017/craigslist_app"
-app.config["MONGO_URI"] = "mongodb+srv://<username>:<password>@cluster0-v6uxh.mongodb.net/complete_data_death"
+
+app.config["MONGO_URI"] = "mongodb+srv://Harmeet:harmeet123@cluster0-v6uxh.mongodb.net/complete_death_coord_data"
 mongo = PyMongo(app)
-​
-# Or set inline
-# mongo = PyMongo(app, uri="mongodb://localhost:27017/craigslist_app")
-​
-​
+
+
 @app.route("/")
 def index():
-    collection = mongo.db.collection.find_one()
+    collection = mongo.db.mortality_records.find_one()
     return render_template("index.html", collection=collection)
-​
-​
-# @app.route("/scrape")
-# def scraper():
-#     listings = mongo.db.listings
-#     listings_data = scrape_craigslist.scrape()
-#     listings.update({}, listings_data, upsert=True)
-#     return redirect("/", code=302)
-​
-​
-if __name__ == "__main__":
-    app.run(debug=True)
+
+##########Route that returns complete data##################
+@app.route('/api/v1.0/data/', methods=['GET'])
+def data():
+  """Return the complete data"""
+  deaths = mongo.db.mortality_records
+  output = []
+  for s in deaths.find():
+    output.append({'year' : s['year'],'cause_name' : s['cause_name'],'state' : s['state'],'deaths' : s['deaths'],'aadr' : s['aadr'], 'Latitude' : s['Latitude'], 'Longitude' : s['Longitude']})
+  return jsonify({'result' : output})
+############################################################
+
+#######Route to return metadata for a year####################
+@app.route("/api/v1.0/metadata/<year>", methods=['GET'])
+def sample_yearData(year):
+    """Return the MetaData for a given year."""
+    query = year
+    data=mongo.db.mortality_records.find({"year": query})
+    years_output = []
+    for i in data:
+        years_output.append({'year' : i['year'],'cause_name' : i['cause_name'],'state' : i['state'],'deaths' : i['deaths']})
+    return jsonify({'result' : years_output})
+###############################################################
+
+#######Route to return metadata for a state####################
+@app.route("/api/v1.0/<state>", methods=['GET'])
+def sample_stateData(state):
+    """Return the MetaData for a given state."""
+    query = state
+    state_data=mongo.db.mortality_records.find({"state": query})
+    state_output = []
+    for i in state_data:
+        state_output.append({'state' : i['state'],'cause_name' : i['cause_name'],'deaths' : i['deaths'],'year' : i['year']})
+    return jsonify({'result' : state_output})
+###############################################################
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
