62f5bf9e1dae34ceb8f3b9cd5e245da373ec0273
murthymalini2019 <53497733+murthymalini2019@users.noreply.github.com>
2019-12-26 22:31:06 -0500

    Adding base layers

--- paradise/static/js/heatmap.js
+++ paradise/static/js/heatmap.js
@@ -1,6 +1,40 @@
+ // Define base layer maps
+
+ var satmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.satellite",
+  accessToken: API_KEY
+});
+
+var lightmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.light",
+  accessToken: API_KEY
+});
+
+var outmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.outdoors",
+  accessToken: API_KEY
+});
+
+// Define a baseMaps object to hold our base layers
+var baseMaps = {
+  "Satellite Map": satmap,
+  "Grayscale Map": lightmap,
+  "Outdoors Map": outmap
+};
+
+// Create overlay object to hold our overlay layer
+var overlayMaps = {
+  "All Causes (2017)": causes
+};
 var myMap = L.map("map", {
   center: [37.0902, -95.7129],
-  zoom: 5
+  zoom: 4
 });
 
 L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
@@ -11,25 +45,47 @@ L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
 }).addTo(myMap);
 
 var url = "/causes";
-
-d3.json(url, function(response) {
-
-  console.log(response);
+d3.json(url, function (response) {
+  var fatality = response.result;
+  console.log(fatality)
 
   var heatArray = [];
+  var latitude = 0;
+  var longitude = 0;
+  var aadr = 0;
+  var marker = [];
+  var state = "";
 
-  for (var i = 0; i < response.length; i++) {
-    var latitude = response[i].latitude;
-    var longitude = response[i].longitude;
+  var markers = L.markerClusterGroup();
 
-    if (latitude) {
-      heatArray.push([latitude, longitude]);
-    }
+  for (var i = 0; i < fatality.length; i++) {
+    latitude = fatality[i].Latitude;
+    longitude = fatality[i].Longitude;
+    state = fatality[i].state;
+    deaths =   fatality[i].deaths;
+    aadr = fatality[i].aadr;
+    theYear = fatality[i].year;
+
+    // marker = L.marker([latitude, longitude]).addTo(myMap); 
+    console.log(latitude,longitude,aadr,state)
+    markers.addLayer(L.marker([latitude, longitude])
+      .bindPopup("<h1>" +state+"</h1><hr><p>Total Deaths in " + theYear + ": "+deaths+"</p><p>Deaths per 100,000 in population: "+aadr+"</p>"));
+
+
+
   }
 
+  // Add our marker cluster layer to the map
+  myMap.addLayer(markers);
+});
+
+
+    
+  //   if (latitude) {
+  //     heatArray.push([latitude, longitude]);
+  //   }
+
   // var heat = L.heatLayer(heatArray, {
-  //   radius: 20,
+  //   radius: aadr*0.10,
   //   blur: 35
   // }).addTo(myMap);
-
-});
