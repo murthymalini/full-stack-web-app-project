60baad31f27d9a59d6e9c2eb45641ac9544d7388
Mary Brown <maryadbrown@yahoo.com>
2019-12-28 12:45:45 -0500

    add Harmeets data routes

--- paradise/app.py
+++ paradise/app.py
@@ -1,30 +1,27 @@
-# import necessary libraries
-# from flask_table import Table, Col
 from flask import Flask, render_template
+# from flask_table import Table, Col
+from flask_pymongo import PyMongo
 from flask import jsonify
 from flask import request
-from flask_pymongo import PyMongo
 import json
+
 import dns
-import os
 
 app = Flask(__name__)
 
-mapkey = os.environ.get('MAPKEY', '')
-
-# Use flask_pymongo to set up mongo connection
-# app.config["MONGO_URI"] = "mongodb://localhost:27017/craigslist_app"
 app.config["MONGO_URI"] = "mongodb+srv://Harmeet:harmeet123@cluster0-v6uxh.mongodb.net/complete_death_coord_data"
 mongo = PyMongo(app)
 
-# Or set inline
-# mongo = PyMongo(app, uri="mongodb://localhost:27017/craigslist_app")
-
+# Route for homepage
 @app.route("/")
 def index():
-    collection = mongo.db.mortality_records.find_one()
-    return render_template("index.html", collection=collection)
-    
+  # """Return the homepage."""
+  collection = mongo.db.mortality_records.find_one()
+  return render_template("index.html", collection=collection)
+############################################################
+
+
+##########Route that returns one year of data for All Causes ##################    
 @app.route('/data', methods=['GET'])
 def getData():
   deaths = mongo.db.mortality_records
@@ -34,7 +31,10 @@ def getData():
   for s in deaths.find(myquery):
     output.append({'year' : s['year'],'cause_name' : s['cause_name'],'state' : s['state'],'deaths' : s['deaths'],'aadr' : s['aadr'], 'Latitude' : s['Latitude'], 'Longitude' : s['Longitude']})
   return jsonify({'result' : output})
+############################################################
 
+
+##########Route that returns distinct causes for drop down menu ##################    
 @app.route("/causes", methods=['GET'])
 def getCauses():
   deaths = mongo.db.mortality_records
@@ -43,7 +43,10 @@ def getCauses():
   for s in deaths.distinct("cause_name"):
     output.append({'cause_name' : s})
   return jsonify({'result' : output})
+############################################################
 
+
+##########Route that returns distinct years for drop down menu ##################    
 @app.route("/years", methods=['GET'])
 def getYears():
 
@@ -53,18 +56,95 @@ def getYears():
   for s in deaths.distinct("year"):
     output.append({'year' : s})
   return jsonify({'result' : output})
+############################################################
 
+
+##########Route that returns just the US map html ##################    
 @app.route("/usmap")
 def usmap():
     collection = mongo.db.mortality_records.find_one()
     return render_template("index_map.html", collection=collection)
+############################################################
 
-# create route that gives us our map key
-@app.route("/mapkey")
-def mapkeyroute():
-    global mapkey
-    config = { "apikey": mapkey }
-    return jsonify(config)
+
+##########Route that returns just the pie chart html ##################    
+@app.route("/piechart")
+def piechart():
+    collection = mongo.db.mortality_records.find_one()
+    return render_template("index_map.html", collection=collection)
+############################################################
+
+
+##########Route that returns complete data##################
+@app.route('/alldata', methods=['GET'])
+def alldata():
+  """Return the complete data"""
+  deaths = mongo.db.mortality_records
+  output = []
+  for s in deaths.find():
+    output.append({'year' : s['year'],'cause_name' : s['cause_name'],'state' : s['state'],'deaths' : s['deaths'],'aadr' : s['aadr'], 'Latitude' : s['Latitude'], 'Longitude' : s['Longitude']})
+  return jsonify({'result' : output})
+
+############################################################
+
+
+#######Route to return metadata for a year####################
+@app.route("/year/<year>", methods=['GET'])
+def sample_yearData(year):
+    """Return the MetaData for a given year."""
+    query = year
+    data=mongo.db.mortality_records.find({"year": int(query)})
+    years_output = []
+    for i in data:
+        years_output.append({'year' : i['year'],'cause_name' : i['cause_name'],'state' : i['state'],'deaths' : i['deaths']})
+    return jsonify({'result' : years_output})
+###############################################################
+
+#######Route to return metadata for a state####################
+@app.route("/state/<state>", methods=['GET'])
+def sample_stateData(state):
+    """Return the MetaData for a given state."""
+    query = state
+    state_data=mongo.db.mortality_records.find({"state": query})
+    state_output = []
+    for i in state_data:
+        state_output.append({'state' : i['state'],'cause_name' : i['cause_name'],'deaths' : i['deaths'],'year' : i['year']})
+    return jsonify({'result' : state_output})
+###############################################################
+##############Route to send data for line plot################
+@app.route("/total_deaths", methods=['GET'])
+def total_deaths():
+  grouped_data=mongo.db.mortality_records.aggregate(
+    [
+      {
+        '$group':
+           {
+               '_id':
+               {'cause': "$cause_name", 
+               'years_name': "$year"},
+              'total_ui': { '$sum': "$deaths" },
+              'count_ui': { '$sum': 1 }
+          }
+      }
+    ]
+  )
+         
+  total_deaths=[]
+  causes=[]
+  years=[]
+  complete_data=[]
+  for i in grouped_data:
+        data={}
+        total_deaths.append(i['total_ui'])
+        causes.append(i['_id']['cause'])
+        years.append(i['_id']['years_name'])
+        data["deaths"]=i['total_ui']
+        data["causes"]=i['_id']['cause']
+        data["year"]=i['_id']['years_name']
+        complete_data.append(data)
+  #Sort data based off year       
+  final_data=sorted(complete_data, key = lambda i: i['year'])     
+  return jsonify(final_data)  
 
 
 if __name__ == "__main__":
