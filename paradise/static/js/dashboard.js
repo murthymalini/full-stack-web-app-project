a88d0a56f398685ea2f24d7429658362739da3bd
murthymalini2019 <53497733+murthymalini2019@users.noreply.github.com>
2020-01-03 20:03:49 -0500

    making slight changes to globalVariables.js

--- paradise/static/js/dashboard.js
+++ paradise/static/js/dashboard.js
@@ -1,75 +1,102 @@
-// var currentCause = "All causes";        
-// var currentYear = "2017";
-
 function init() {
-    // Grab a reference to the dropdown select element
-    var selectorCause = d3.select("#selCauseDataset");
-    var selectorYear = d3.select("#selYearDataset");
-  
-    // Use the list of distinct causes to populate the select options
-    var url = "/causes";
-    d3.json(url).then(function (response) {
-      var causeList = response.result;
-      causeList.forEach((cause) => {
-        selectorCause
-          .append("option")
-          .text(cause.cause_name)
-          .property("value", cause.cause_name)
-          .enter;
-      });  
+  // Grab a reference to the dropdown select element
+  var selectorCause = d3.select("#selCauseDataset");
+  var selectorYear = d3.select("#selYearDataset");
+  var selectorMeasure = d3.select("#selSummary");
+
+  // Use the list of distinct causes to populate the select options
+  var url = "/causes";
+  d3.json(url).then(function (response) {
+    var causeList = response.result;
+    causeList.forEach((cause) => {
+      selectorCause
+        .append("option")
+        .text(cause.cause_name)
+        .property("value", cause.cause_name)
+        .enter;
     });
-  
-    d3.select("#selCauseDataset").select("option").property("value",currentCause)
-      .text(currentCause);
-  
-    // Use the list of distinct years  to populate the select options
-    var url = "/years";  
-    d3.json(url).then(function (response) {
-      var yearList = response.result;
-      yearList.forEach((year) => {
-        selectorYear
-          .append("option")
-          .text(year.year)
-          .property("value", year.year);
-      });      
-    }); 
-  
-    buildDonutChart(currentYear);
-    buildColorMap(currentCause,currentYear);
-  
-    // buildBasicMap(currentCause,currentYear);
-    // buildPieCharts(currentYear);
-    // buildBarChart(currentYear);
-    // buildTestCharts(currentCause,currentYear);
-    // buildLayeredMap(currentCause,currentYear);
-    // buildLineChart();
-    
+  });
+
+  selectorCause.select("option").property("value", currentCause)
+    .text(currentCause);
+
+  // Use the list of distinct years  to populate the select options
+  var url = "/years";
+  d3.json(url).then(function (response) {
+    var yearList = response.result;
+    yearList.forEach((year) => {
+      selectorYear
+        .append("option")
+        .text(year.year)
+        .property("value", year.year);
+    });
+  });
+
+  selectorYear.select("option").property("value", currentYear)
+    .text(currentYear);
+
+  selectorMeasure
+    .append("option")
+    .text("AADR")
+    .property("value", "aadr")
+    .enter;
+
+  selectorMeasure
+    .append("option")
+    .text("Total Deaths")
+    .property("value", "deaths")
+    .enter;
+
+
+
+  selectorMeasure.select("option").property("value", "aadr")
+    .text("AADR");
+
+  buildDonutChart(currentYear, currentSummary);
+
+  // setDeathRate(currentCause,currentYear,currentSummary);
+  buildColorMap(currentCause, currentYear, currentSummary);
+
+}
+
+function optionChangeCause(newCause) {
+  // Fetch new data each time a new cause is selected
+  currentCause = newCause;
+  // setDeathRate(currentCause,currentYear,currentSummary);
+  buildColorMap(currentCause, currentYear, currentSummary);
+
+}
+function optionChangeYear(newYear) {
+  // Fetch new data each time a new year is selected
+  currentYear = newYear;
+
+  resetDonutCanvas();
+  buildDonutChart(currentYear, currentSummary);
+
+  // setDeathRate(currentCause,currentYear,currentSummary);
+  buildColorMap(currentCause, currentYear, currentSummary);
+}
+
+function optionChangeSummary(newSummary) {
+  // Fetch new data each time a new summary is selected
+  currentSummary = newSummary;
+
+  if (currentSummary == 'aadr') {
+    currentUnits = " aadr";
+    chartTitle = currentYear + " Age Adjusted Death Rate (per 100,000 in population)";
   }
-    
-  function optionChangeCause(newCause) {
-      // Fetch new data each time a new cause is selected
-      currentCause = newCause;
-  
-      buildColorMap(currentCause,currentYear);
-  
-      // buildBasicMap(currentCause,currentYear);
-      // buildLayeredMap(currentCause,currentYear);
-      // buildLineChart();
+  else {
+    currentUnits = " deaths";
+    chartTitle = currentYear + " Total Deaths";
   }
-  function optionChangeYear(newYear) {
-      // Fetch new data each time a new year is selected
-      currentYear = newYear;
-  
-      buildDonutChart(currentYear);
-      buildColorMap(currentCause,currentYear);
-  
-      // buildPieCharts(currentYear);
-      // buildBarChart(currentYear);
-      // buildBasicMap(currentCause,currentYear);
-      // buildLayeredMap(currentCause,currentYear);
-      // buildLineChart();
-  }  
-  
-  // Initialize the dashboard
-  init();
-  
+
+  resetDonutCanvas();
+  buildDonutChart(currentYear, currentSummary);
+
+  setDeathRate(currentCause, currentYear, currentSummary);
+  buildColorMap(currentCause, currentYear, currentSummary);
+}
+
+// Initialize the dashboard
+
+init();
---
+++ paradise/static/js/globalVariables.js
@@ -0,0 +1,5 @@
+var currentCause = "All causes";        
+var currentYear = "2017";     
+var currentSummary = "aadr"; 
+var currentUnits  = " aadr";
+var chartTitle = currentYear + " Age Adjusted Death Rate (per 100,000 in population)";
